56e18080b0a7edcdb2916df8c16864f0814c4fa7
subhash nagula <subhashdeveloper22@gmail.com>
2025-06-09 12:13:29 +0530

    tm using spring additional change

--- src/main/java/com/travel/App.java
+++ src/main/java/com/travel/App.java
@@ -1,9 +1,13 @@
 package com.travel;
 
+import java.util.Arrays;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.travel.model.Bus;
 import com.travel.model.Flight;
+import com.travel.model.Train;
 import com.travel.model.User;
 
 /**
@@ -17,10 +21,27 @@ public class App
 
     	ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/travel/files/travelconfig.xml");
     	
-    	User user=applicationContext.getBean(User.class);
-    	Flight flight=applicationContext.getBean(Flight.class);
+    	System.out.println(applicationContext.getBeanDefinitionCount());
     	
-    	user.setRedbus(flight);
+    	System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
+    	
+    	
+    	User user=applicationContext.getBean(User.class);
+    	
+    	Flight flight=applicationContext.getBean(Flight.class);
+ 
+    	
+    	Bus bus=applicationContext.getBean(Bus.class);
+    	
+    	Train train =applicationContext.getBean(Train.class);
+    	
+    	//user.setRedbus(flight);//here jvm is injection object to interface , but i need to make ioc container to do it.
+    	//to i need to use propery tag in xml config, property tag is due to iserting values using setter, if we insert use constructor we use other tags.
+    	//**** Note: In Spring configuration, we use the <property> tag inside a <bean> to inject dependencies or initialize properties.
+
+    	//About dependency injection can be finished by ioc container.
+    	
+    	//So, we dont need user.setRedbus(flight);
     	
     	user.reachDestination();//NullPointerException(before adding implementation class , interface bean is empty)
     	//so we need to create object for interface first.
